bfe91cce59b9d4a1980bd3ee6c76f682dcf4fc4f
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-20 18:34:44 +0100

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+import { Home, CreatePost } from './pages'
 
-function App() {
+const App = () => {
 
   return (
-      <h1 className='text-3xl font-bold underline text-red-500'>
-        Hello
-      </h1>
+    <BrowserRouter>
+      <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b-[#E6EBF4]">
+        <Home />
+        <CreatePost />
+      </header>
+    </BrowserRouter>
   )
 }
 
