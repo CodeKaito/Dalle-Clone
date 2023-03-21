f518f8f9c2dd4595a299eae82304d9b16b701100
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-21 13:23:12 +0100

    Update CreatePost.jsx

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -13,7 +13,7 @@ const CreatePost = () => {
     prompt:'',
     photo:'',
   });
-  const [generatingImg, setGeneratingImg] = useState(false);
+  const [generatingImg, setGeneratingImg] = useState(true);
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = () => {
@@ -39,6 +39,20 @@ const CreatePost = () => {
           <div className='flex flex-col gap-5'>
             <FormField labelName='Your name' text='text' name='name' placeholder='John Doe' value={form.name} handleChange={handleChange} />
             <FormField labelName='Prompt' text='text' name='prompt' placeholder='A futuristic cyborg dance club, neon lights' value={form.prompt} handleChange={handleChange} isSurpriseMe handleSupriseMe={handleSupriseMe} />
+          
+            <div className='relative bg-gray-50 border border-gray-4300 text-gray-900 text-sm rounded-lg focus:ring-blue-600 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center'>
+              {form.photo ? (
+                <img src={form.photo} alt={form.prompt} className='w-full h-full object-contain' />
+              ) : (
+                <img src={preview} alt='preview' className='w-9/12 h-9/12 object-contain opacity-40' />
+              )}
+
+              {generatingImg && (
+                <div className='absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg'>
+                  <Loader />
+                </div>
+              )}
+            </div>
           </div>
         </form>
     </section>
