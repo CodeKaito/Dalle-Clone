4ab9628e2d2ddb5bd393792ec347cb4f84d88ebb
CodeKaito <jerome.decinco@maxwell.mi.it>
2023-03-24 17:14:54 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/post', postRoutes);
+app.use('api/v1/dalle', dalle);
+
 app.get('/', async (req, res) => {
     res.send('Hello from DALL-E');
 })
